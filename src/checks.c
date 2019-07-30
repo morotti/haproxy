0cb72e420839aad6460dcf54d07e3e2c7e9e0b1a
morotti <r.morotti@gmail.com>
2019-07-30 21:09:50 +0100

    fix missing braces in if block
    Add missing braces around the if(s->vhost) block. Add braces in nearby sections to clarify the intention.

--- src/checks.c
+++ src/checks.c
@@ -1584,16 +1584,22 @@ static int connect_conn_chk(struct task *t)
 		}
 		else if ((check->type) == PR_O2_HTTP_CHK) {
 			if (s->proxy->options2 & PR_O2_CHK_SNDST)
+			{
 				b_putblk(&check->bo, trash.area,
 					 httpchk_build_status_header(s, trash.area, trash.size));
+			}
 			/* set the host header per backend */
 			if (s->vhost)
+			{
 				b_putist(&check->bo, ist("Host: "));
 				b_putist(&check->bo, ist(s->vhost));
 				b_putist(&check->bo, ist("\r\n"));
+			}
 			/* prevent HTTP keep-alive when "http-check expect" is used */
 			if (s->proxy->options2 & PR_O2_EXP_TYPE)
+			{
 				b_putist(&check->bo, ist("Connection: close\r\n"));
+			}
 			b_putist(&check->bo, ist("\r\n"));
 			*b_tail(&check->bo) = '\0'; /* to make gdb output easier to read */
 		}
